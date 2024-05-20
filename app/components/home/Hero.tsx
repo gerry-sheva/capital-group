e59ca245692af334a970b4a8457290fdc187eb8d
gerry-sheva <gerry.sheva@proton.me>
2024-05-20 10:45:35 +0700

    feat: testing video hero

--- app/components/home/Hero.tsx
+++ app/components/home/Hero.tsx
@@ -1,13 +1,16 @@
 import Image from 'next/image'
 import Hero from '../Hero'
+import { Suspense } from 'react'
+import { list } from '@vercel/blob'
 
 const HeroHome: React.FC = () => {
 	return (
 		<Hero>
-			<h1 className="text-4xl lg:text-7xl my-8 lg:my-16">
+			<Suspense fallback={<p>Loading video...</p>}>
+				<VideoComponent fileName="cg-dgaJih8v6LlJ5lktms3MUu3Q88aSGj.mp4" />
+			</Suspense>
+			{/* <h1 className="text-4xl lg:text-7xl my-8 lg:my-16">
 				BIG GOALS?
-				{/* <br /> */}
-				{/* COMPLEX PROJECTS? */}
 				<br />
 				WHEN RESULTS MATTER,
 				<br />
@@ -30,9 +33,32 @@ const HeroHome: React.FC = () => {
 					<br />
 					to achieve stellar results.
 				</h2>
-			</div>
+			</div> */}
 		</Hero>
 	)
 }
 
 export default HeroHome
+
+async function VideoComponent({ fileName }) {
+	const { blobs } = await list({
+		prefix: fileName,
+		limit: 1,
+	})
+	// console.log(blobs)
+	const { url } = blobs[0]
+
+	return (
+		<video
+			controls
+			preload="none"
+			aria-label="Video player"
+		>
+			<source
+				src={url}
+				type="video/mp4"
+			/>
+			Your browser does not support the video tag.
+		</video>
+	)
+}
